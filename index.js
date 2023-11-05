732ed330e2d3b7cafe854caf540d973023520d7f
Andriy Kupchanko <80577186+andriykupchanko@users.noreply.github.com>
2023-11-05 12:16:20 +0200

    dd

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
 const { Telegraf } = require('telegraf');
 
-const token = '6596788820:AAHG95BPmrbEkaeU7SAO_auKHaYIEpW4_T0'; 
+const token = '6645546714:AAHosE0btx0a8N8YfaGDr-mEUQ1ymQkDuDs'; 
 
 const bot = new Telegraf(token);
+bot.start( async (ctx) => {
+  // Get the username of the current user
+  const username = ctx.from.username;
 
-const messageLimits = new Map(); // Лічильник кількості повідомлень від користувачів
-const messageLimit = 3; // Максимальна кількість повідомлень на секунду
-const timeout = 1000; // Тайм-аут в мілісекундах (1000 мс = 1 секунда)
+  // Welcome the user
+  await ctx.reply(`Hello, ${username}! Welcome to my bot!`);
 
-bot.on('text', async (ctx) => {
-  const chatId = ctx.chat.id;
-  const userId = ctx.from.id;
+  // Send the user a list of commands
+  await ctx.reply("/help - List of commands");
+  await ctx.reply("/weather - Get the weather forecast");
+  await ctx.reply("/news - Get the latest news headlines");
+});
+bot.command("week", async (ctx) => {
 
-  // Перевірка, чи є користувач в мапі лічильника
-  if (!messageLimits.has(userId)) {
-    messageLimits.set(userId, 0);
-  }
-
-  // Отримання поточного лічильника для користувача
-  const messageCount = messageLimits.get(userId);
-
-  // Перевірка, чи не перевищена максимальна кількість повідомлень
-  if (messageCount >= messageLimit) {
-    ctx.reply('Ви надіслали занадто багато повідомлень. Зачекайте трохи.');
-    return;
-  }
-
-  // Збільшення лічильника для користувача
-  messageLimits.set(userId, messageCount + 1);
-
-  // Очікування тайм-ауту перед зняттям лічильника
-  setTimeout(() => {
-    messageLimits.set(userId, messageCount - 1);
-  }, timeout);
-
-  // Обробка повідомлення
-  ctx.reply('Ваше повідомлення було оброблено.');
+  await ctx.reply("/help - List of commands");
+ 
+});
+bot.command("stop", async (ctx) => {
+    await ctx.reply("Goodbye!");
 });
 
+bot.command("strawberry", async (ctx) => {
+      await ctx.reply('https://t.me/pandahikkan');
+});
 bot.launch();
